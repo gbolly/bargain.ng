fa9314c39112456657bf3aba0da9dd9a6f1551c8
andela-gokerayi <gbolahan.okerayi@andela.com>
2017-12-15 00:07:31 +0100

    fix

--- bargain/settings/base.py
+++ bargain/settings/base.py
@@ -118,6 +118,14 @@ USE_L10N = True
 
 USE_TZ = True
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = envvars.get('EMAIL_HOST')
+EMAIL_HOST_USER = envvars.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = envvars.get('EMAIL_HOST_PASSWORD')
+EMAIL_PORT = 465
+EMAIL_USE_SSL = True
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
